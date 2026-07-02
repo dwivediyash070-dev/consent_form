0aed3c60d0d519fd92dab6fea6000b23ab500fd0
dwivediyash070-dev <dwivediyash070@gmail.com>
2026-07-02 22:47:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("consentForm").addEventListener("submit",function(e){
+e.preventDefault();
+const data={};
+new FormData(this).forEach((v,k)=>data[k]=v);
+console.log(data);
+document.getElementById("message").textContent="Form submitted successfully!";
+this.reset();
+});
